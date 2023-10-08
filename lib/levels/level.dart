8007b07733a81161439040ef7b04ab2caef2dd0e
Quinn Barnett <squinnmb@gmail.com>
2023-10-07 22:13:52 -0500

    ugh

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -1,22 +1,33 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:gain/actors/player.dart';
-import 'package:gain/levels/collision_block.dart';
+import 'package:gain/levels/platform.dart';
 
-class Level extends World {
+class Level extends World with HasGameRef {
   String levelName;
-  Player player;
-  Level({required this.levelName, required this.player});
+  Level({required this.levelName});
 
+  late Player player;
   late TiledComponent level;
+  late Rect levelBounds;
 
   @override
   FutureOr<void> onLoad() async {
     level = await TiledComponent.load("$levelName.tmx", Vector2.all(16));
+    levelBounds = Rect.fromLTWH(
+        0, 0, (level.tileMap.map.width * level.tileMap.map.tileWidth).toDouble(), (level.tileMap.map.height * level.tileMap.map.tileHeight).toDouble());
+    Player player = Player(levelBounds: levelBounds);
+    _spawnActors();
+    game.camera.follow(player);
+    _createPlatforms();
     add(level);
-    final spawnPointLayer = level.tileMap.getLayer<ObjectGroup>('SpawnPoints'); // rn this is only one item loop unneccessary
+    return super.onLoad();
+  }
 
+  void _spawnActors() {
+    ObjectGroup? spawnPointLayer = level.tileMap.getLayer<ObjectGroup>('SpawnPoints');
     if (spawnPointLayer != null) {
       for (final spawnPoint in spawnPointLayer.objects) {
         if (spawnPoint.class_ == "Player") {
@@ -25,24 +36,24 @@ class Level extends World {
         }
       }
     }
+  }
 
+  void _createPlatforms() {
     final collisionLayer = level.tileMap.getLayer<ObjectGroup>("Collisions");
     if (collisionLayer != null) {
       for (final collision in collisionLayer.objects) {
         if (collision.class_ == "Platform") {
-          final platform = CollisionBlock(position: Vector2(collision.x, collision.y), size: Vector2(collision.width, collision.height), isPlatform: true);
-
+          // need to change class name
+          final platform = Platform(position: Vector2(collision.x, collision.y), size: Vector2(collision.width, collision.height), isPassable: true);
           add(platform);
         } else {
-          final block = CollisionBlock(
+          final block = Platform(
             position: Vector2(collision.x, collision.y),
             size: Vector2(collision.width, collision.height),
           );
-
           add(block);
         }
       }
     }
-    return super.onLoad();
   }
 }
